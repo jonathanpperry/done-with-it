820fcd4bab28e2619a89ce04eab6b2c4a46a3972
Jonathan Perry <jpper@umich.edu>
2021-01-21 00:20:33 +0900

    Finished welcome screen section

--- App.js
+++ App.js
@@ -8,32 +8,10 @@ import {
   Text,
   View,
 } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <SafeAreaView
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          flexShrink: 1,
-          width: 400,
-          height: 100,
-        }}
-      />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
