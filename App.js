f107f421ed16ac21cf0fceaf8c593e677ad71b50
Jonathan Perry <jpper@umich.edu>
2021-01-20 23:48:04 +0900

    Experimented with detecting orientation changes

--- App.js
+++ App.js
@@ -6,22 +6,30 @@ import {
   StyleSheet,
   SafeAreaView,
   Text,
+  View,
 } from "react-native";
+import {
+  useDeviceOrientation,
+  useDimensions,
+} from "@react-native-community/hooks";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Text>Hello React!</Text>
-      <Button
-        color="blue"
-        title="Click Me!"
-        onPress={() => console.log("Button pressed")}
-      />
+    <SafeAreaView style={[styles.container]}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+        }}
+      ></View>
     </SafeAreaView>
   );
 }
 
-const containerStyle = { backgroundColor: "orange" };
+// const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
