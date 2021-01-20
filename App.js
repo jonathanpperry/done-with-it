192286bfb33c387b47a1e531edf35d48da715816
Jonathan Perry <jpper@umich.edu>
2021-01-20 23:55:47 +0900

    Finished flexDirection section

--- App.js
+++ App.js
@@ -17,10 +17,10 @@ export default function App() {
   const { landscape } = useDeviceOrientation();
 
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row" }}>
+      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
