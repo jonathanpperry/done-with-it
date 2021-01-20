74178bd99a5f175cd7f508815561bc1c8f8081a5
Jonathan Perry <jpper@umich.edu>
2021-01-20 22:53:33 +0900

    Changed background to blue and used a SafeAreaView

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
-  let x = 1;
-  console.log("App executed");
-
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "dodgerblue",
   },
 });
